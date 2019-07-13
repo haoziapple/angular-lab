c991077fd57a3ab00f9b26ec7b5112288e879f44
haoziapple <haozixiaowang@163.com>
2019-07-13 16:18:15 +0800

    使用Service和rxjs：Observable，of，subscribe的使用

--- collection-app/src/app/heros/heros.component.ts
+++ collection-app/src/app/heros/heros.component.ts
@@ -1,4 +1,4 @@
-import { HEROES } from './../mock-heroes';
+import { HeroService } from './../service/hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
@@ -14,17 +14,24 @@ export class HerosComponent implements OnInit {
     name: 'Wind Storm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    // this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes)
   }
 
 }
